801aa5866be37ed36ee47f15cae87e7c36292ae7
lotus64yt <lotus64santon@gmail.com>
2026-04-08 16:02:50 +0200

    fix next-inlt error

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,15 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 try {
-  require('./server/timer-bootstrap.cjs');
+  require("./server/timer-bootstrap.cjs");
 } catch (e) {
-  console.error('[next.config] timer error', e);
+  console.error("[next.config] timer error", e);
 }
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
 export default withNextIntl(nextConfig);
-
